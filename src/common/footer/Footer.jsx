949dcb2b162da9a0af2c421929be9504397e3ff7
LeHoangMinh31 <le31012000@gmail.com>
2023-04-18 18:22:14 +0700

    leminh

---
+++ src/common/footer/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container grid2'>
+          <div className='box'>
+            <h1>Bonik</h1>
+            <p>Tải app ngay tại.</p>
+            <div className='icon d_flex'>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-google-play'></i>
+                <span>Google Play</span>
+              </div>
+              <div className='img d_flex'>
+                <i class='fa-brands fa-app-store-ios'></i>
+                <span>App Store</span>
+              </div>
+            </div>
+          </div>
+
+          <div className='box'>
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h2>Liên Hệ</h2>
+            <ul>
+              <li>63A, Võ Văn Tần, Q. 3, Tp. Hồ Chí Minh </li>
+              <li>Email: le31012000@gmail.com</li>
+              <li>Phone: 0978642331</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/MainPage/Categories.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+
+const Categories = () => {
+  const data = [
+    {
+      cateImg: "./images/category/cat1.png",
+      cateName: "Fashion",
+    },
+    {
+      cateImg: "./images/category/cat2.png",
+      cateName: "Electronic",
+    },
+    {
+      cateImg: "./images/category/cat5.png",
+      cateName: "Gifts",
+    },
+    {
+      cateImg: "./images/category/cat8.png",
+      cateName: "Fashion Pets",
+    },
+    {
+      cateImg: "./images/category/cat9.png",
+      cateName: "Fashion Baby",
+    },
+    {
+      cateImg: "./images/category/cat11.png",
+      cateName: "Accessory",
+    },
+  ]
+
+  return (
+    <>
+      <div className='category'>
+        {data.map((value, index) => {
+          return (
+            <div className='box f_flex' key={index}>
+              <img src={value.cateImg} alt='' />
+              <span>{value.cateName}</span>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Categories
