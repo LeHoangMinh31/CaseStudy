5af3be40645cecf7efa716b1c3ee38ebdae0ae6a
LeHoangMinh31 <78464622+LeHoangMinh31@users.noreply.github.com>
2023-04-24 12:03:15 +0700

    Update Cart.jsx

--- src/common/Cart/Cart.jsx
+++ src/common/Cart/Cart.jsx
@@ -1,50 +1,49 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./style.css"
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
+import { CartContext } from "../../contexts/CartContext"
 
+const Cart = ({ increaseQty, decreaseQty, deleteCartItem }) => {
 
-const Cart = ({ CartItem, addToCart, decreaseQty }) => {
-  
-  const totalPrice = CartItem.reduce((price, item) => price + item.qty * item.price, 0)
+  const { cartItems } = useContext(CartContext)
 
-  
-  
+  const totalPrice = cartItems.reduce((price, item) => price + item.qty * item.productPrice, 0)
+
   return (
     <>
       <section className='cart-items'>
         <div className='container d_flex'>
-          
 
           <div className='cart-details'>
-            {CartItem.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
+            {cartItems.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
 
-            
-            {CartItem.map((item) => {
-              const productQty = item.price * item.qty
+            {cartItems.map((item) => {
+              const productQty = item.productPrice * item.qty
 
               return (
                 <div className='cart-list product d_flex' key={item.id}>
                   <div className='img'>
-                    <img src={item.cover} alt='' />
+                    <img src={item.productImage} alt='' />
                   </div>
                   <div className='cart-details'>
-                    <h3>{item.name}</h3>
+                    <h3>{item.productName}</h3>
                     <h4>
-                      ${item.price}.00 * {item.qty}
+                      ${item.productPrice}.00 * {item.qty}
                       <span>${productQty}.00</span>
                     </h4>
                   </div>
                   <div className='cart-items-function'>
                     <div className='removeCart'>
-                      <button className='removeCart'>
+                      <button className='removeCart' onClick={() => deleteCartItem(item.id)}>
                         <i className='fa-solid fa-xmark'></i>
                       </button>
                     </div>
-                    
+
                     <div className='cartControl d_flex'>
                       <button className='desCart' onClick={() => decreaseQty(item)}>
                         <i className='fa-solid fa-minus'></i>
                       </button>
-                      <button className='incCart' onClick={() => addToCart(item)}>
+                      <button className='incCart' onClick={() => increaseQty(item)}>
                         <i className='fa-solid fa-plus'></i>
                       </button>
                     </div>
@@ -56,13 +55,28 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
             })}
           </div>
 
-          <div className='cart-total product'>
-            <h2>Cart Summary</h2>
-            <div className=' d_flex'>
-              <h4>Tổng :</h4>
-              <h3>${totalPrice}.00</h3>
+          <div style={{ display: 'flex', flexDirection: 'column' }} className="d_flex cart-total product">
+            <div>
+              <div>
+                <h2>Cart Summary</h2>
+                <div className='d_flex'>
+                  <h4>Tá»•ng :</h4>
+                  <h3>${totalPrice}.00</h3>
+                </div>
+              </div>
             </div>
+            {
+              cartItems.length
+                ? (
+                  <Link className="btn btn-primary" style={{ cursor: 'pointer', marginTop: '40px', textAlign: 'center' }} to='/check-out'>
+                    Check out
+                  </Link>
+                ) : (
+                  <></>
+                )
+            }
           </div>
+
         </div>
       </section>
     </>
