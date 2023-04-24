4afb735705ebc12ebbaca1e3f85521ac19144b2c
LeHoangMinh31 <78464622+LeHoangMinh31@users.noreply.github.com>
2023-04-24 12:22:58 +0700

    Update TopCart.jsx

--- src/components/top/TopCart.jsx
+++ src/components/top/TopCart.jsx
@@ -4,30 +4,30 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Tdata from "./Tdata"
 
+const settings = {
+  dots: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+}
+
 const TopCart = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-  }
+
   return (
     <>
       <Slider {...settings}>
         {Tdata.map((value, index) => {
           return (
-            <>
-              <div className='box product' key={index}>
-                <div className='nametop d_flex'>
-                  <span className='tleft'>{value.para}</span>
-                  <span className='tright'>{value.desc}</span>
-                </div>
-                <div className='img'>
-                  <img src={value.cover} alt='' />
-                </div>
+            <div className='box product' key={index}>
+              <div className='nametop d_flex'>
+                <span className='tleft'>{value.para}</span>
+                <span className='tright'>{value.desc}</span>
               </div>
-            </>
+              <div className='img'>
+                <img src={value.image} alt='' />
+              </div>
+            </div>
           )
         })}
       </Slider>
@@ -36,3 +36,4 @@ const TopCart = () => {
 }
 
 export default TopCart
+
