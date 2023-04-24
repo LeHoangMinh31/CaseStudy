25d9f5a9e2987f3c18563332b47504e6b0d9b5d8
LeHoangMinh31 <78464622+LeHoangMinh31@users.noreply.github.com>
2023-04-24 12:17:45 +0700

    Update FlashCard.jsx

--- src/components/flashDeals/FlashCard.jsx
+++ src/components/flashDeals/FlashCard.jsx
@@ -1,84 +1,45 @@
 import React, { useState } from "react"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
 
-const SampleNextArrow = (props) => {
-  const { onClick } = props
-  return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='next'>
-        <i className='fa fa-long-arrow-alt-right'></i>
-      </button>
-    </div>
-  )
-}
-const SamplePrevArrow = (props) => {
-  const { onClick } = props
-  return (
-    <div className='control-btn' onClick={onClick}>
-      <button className='prev'>
-        <i className='fa fa-long-arrow-alt-left'></i>
-      </button>
-    </div>
-  )
-}
-const FlashCard = ({ productItems, addToCart }) => {
+
+const FlashCard = ({ productItem, addToCart }) => {
+
   const [count, setCount] = useState(0)
+
   const increment = () => {
     setCount(count + 1)
   }
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-  }
 
   return (
-    <>
-      <Slider {...settings}>
-        {productItems.map((productItems) => {
-          return (
-            <div className='box'>
-              <div className='product mtop'>
-                <div className='img'>
-                  <span className='discount'>{productItems.discount}% Off</span>
-                  <img src={productItems.cover} alt='' />
-                  <div className='product-like'>
-                    <label>{count}</label> <br />
-                    <i className='fa-regular fa-heart' onClick={increment}></i>
-                  </div>
-                </div>
-                <div className='product-details'>
-                  <h3>{productItems.name}</h3>
-                  <div className='rate'>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                    <i className='fa fa-star'></i>
-                  </div>
-                  <div className='price'>
-                    <h4>${productItems.price}.00 </h4>
-                    {/* step : 3  
-                     if hami le button ma click garryo bahne 
-                    */}
-                    <button onClick={() => addToCart(productItems)}>
-                      <i className='fa fa-plus'></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )
-        })}
-      </Slider>
-    </>
+    <div className='box' key={productItem.id}>
+      <div className='product mtop'>
+        <div className='img'>
+          <span className='discount'>{productItem.discount}% Off</span>
+          <img src={productItem.image} alt='' />
+          <div className='product-like'>
+            <label>{count}</label> <br />
+            <i className='fa-regular fa-heart' onClick={increment}></i>
+          </div>
+        </div>
+        <div className='product-details'>
+          <h3>{productItem.name}</h3>
+          <div className='rate'>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+            <i className='fa fa-star'></i>
+          </div>
+          <div className='price'>
+            <h4>${productItem.price}.00 </h4>
+            <button onClick={() => addToCart(productItem)}>
+              <i className='fa fa-plus'></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default FlashCard
+
